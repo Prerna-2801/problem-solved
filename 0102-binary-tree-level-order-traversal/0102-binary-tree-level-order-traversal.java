60f5492479df01cb916da7273fad50d7ce5475c7
Prerna-2801 <prernaraj2801@gmail.com>
2026-04-20 09:53:03 +0530

    Time: 1 ms (95.14%), Space: 46.9 MB (25.34%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -26,12 +26,8 @@ class Solution {
             for(int i = 0; i<len; i++){
                 TreeNode node = q.poll();
                 ans.get(currLevel).add(node.val);
-                if(node.left != null){
-                    q.offer(node.left);
-                }
-                if(node.right != null){
-                    q.offer(node.right);
-                }
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
             }
             currLevel++;
         }
