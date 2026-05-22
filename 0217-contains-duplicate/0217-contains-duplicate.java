5e42a86f98bab5b110ccd317dc306f6ae28c2bf6
Prerna Raj <prernaraj2801@gmail.com>
2026-05-22 20:47:15 +0530

    Time: 21 ms (31.6%), Space: 80.8 MB (77.86%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int n = nums.length;
+        for(int i = 0; i<n; i++){
+            set.add(nums[i]);
+        }
+        if(set.size() == n) return false;
+        return true;
+    }
+}
