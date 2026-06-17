c639ba7b3ec72045a2d677266d630a3a5be048d6
Prerna Raj <prernaraj2801@gmail.com>
2026-06-17 08:49:18 +0530

    Time: 8 ms (15.11%), Space: 45.2 MB (65.89%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -2,21 +2,21 @@ class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(candidates);
-        f(0, candidates, target, ans, new ArrayList<>());
+        f(ans, 0, new ArrayList<>(), candidates, target, 0);
         return ans;
     }
-    public void f(int index, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds){
-        if(target == 0){
+    public void f(List<List<Integer>> ans, int sum, List<Integer> ds, int[] candidates, int target, int i){
+        if(sum == target){
             ans.add(new ArrayList<>(ds));
             return;
         }
-        if(index == arr.length) return;
-        for(int i = index; i<arr.length; i++){
-            if(i >  index && arr[i] == arr[i-1]) continue;
-            if(arr[i] > target) break;
-            ds.add(arr[i]);
-            f(i+1, arr, target-arr[i], ans, ds);
-            ds.remove(ds.size()-1);
+        if(i >= candidates.length || sum > target) return;
+        ds.add(candidates[i]);
+        f(ans, sum+candidates[i], ds, candidates, target, i+1);
+        ds.remove(ds.size()-1);
+        while(i+1 < candidates.length && candidates[i] == candidates[i+1]){
+            i++;
         }
+        f(ans, sum, ds, candidates, target, i+1);
     }
 }
