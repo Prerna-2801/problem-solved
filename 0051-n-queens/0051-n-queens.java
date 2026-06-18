7998fdd682b0c134ca0b77041f29c290bccc68dc
Prerna Raj <prernaraj2801@gmail.com>
2026-06-18 15:16:33 +0530

    Time: 2 ms (84.35%), Space: 46.8 MB (15.31%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -13,30 +13,25 @@ class Solution {
     public void f(int n, int row, char[][] arr, List<List<String>> ans){
         if(row == n){
             List<String> list = new ArrayList<>();
-            for(char[] c: arr){
+            for(char[] c : arr){
                 list.add(new String(c));
             }
             ans.add(list);
             return;
         }
-        for(int col = 0; col<n; col++){
-            if(isSafe(n, row, col, arr)){
+        for(int col = 0; col <n; col++){
+            if(isPossible(n, row, col, arr)){
                 arr[row][col] = 'Q';
                 f(n, row+1, arr, ans);
                 arr[row][col] = '.';
             }
         }
     }
-    public boolean isSafe(int n, int row, int col, char[][] arr){
-        //same row
+    public boolean isPossible(int n, int row, int col, char[][] arr){
         for(int i = 0; i<n; i++){
             if(arr[i][col] == 'Q') return false;
         }
-        //diagonals
         for(int i = row+1, j = col+1; i<n && j<n; i++, j++){
-            if(arr[i][j] == 'Q') return false;
-        }
-        for(int i = row+1, j = col-1; i<n && j>00; i++, j--){
             if(arr[i][j] == 'Q') return false;
         }
         for(int i = row-1, j = col-1; i>=0 && j>=0; i--, j--){
