0d02e42865d1e56821988918fa697d94a02ffa4e
Prerna Raj <prernaraj2801@gmail.com>
2026-08-25 13:48:11 +0530

    Time: 0 ms (100%), Space: 42.4 MB (17.45%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,9 @@
+class Solution {
+    int[] arr = new int[46];
+    public int climbStairs(int n) {
+        if(n <= 1) return 1;
+        if(arr[n] != 0) return arr[n];
+        arr[n] = climbStairs(n-2) + climbStairs(n-1);
+        return arr[n];        
+    }
+}
