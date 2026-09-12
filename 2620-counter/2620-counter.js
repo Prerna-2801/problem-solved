d8dd9b8808a992556b87ba7e114805e057d5b302
Prerna Raj <prernaraj2801@gmail.com>
2026-09-12 13:06:36 +0530

    Time: 51 ms (12%), Space: 53.5 MB (58.8%) - LeetHub

--- 2620-counter/2620-counter.js
+++ 2620-counter/2620-counter.js
@@ -3,16 +3,12 @@
  * @return {Function} counter
  */
 var createCounter = function(n) {
-    let cnt = 0
+    
     return function() {
-        if(cnt == 0){
-            cnt++
-            return n
-        }
-        else return n+=1
+        return n++;
     };
 };
-createCounter()
+
 /** 
  * const counter = createCounter(10)
  * counter() // 10
